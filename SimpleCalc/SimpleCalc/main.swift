107751e6ad683eafceaac12d546cfe772de4e249
Alex Schenck <AlexNSchenck@gmail.com>
2015-10-07 18:11:03 -0700

    First commit

---
+++ SimpleCalc/SimpleCalc/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SimpleCalc
+//
+//  Created by iGuest on 10/7/15.
+//  Copyright (c) 2015 Alex Schenck. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
