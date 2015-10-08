99be19cb3d75ea92f1b975d259ca94bb9ee3fa25
Alex Schenck <AlexNSchenck@gmail.com>
2015-10-07 18:20:30 -0700

    Now accepts input

--- SimpleCalc/SimpleCalc/main.swift
+++ SimpleCalc/SimpleCalc/main.swift
@@ -8,5 +8,15 @@
 
 import Foundation
 
-println("Hello, World!")
+func input() -> String
+{
+    let keyboard = NSFileHandle.fileHandleWithStandardInput()
+    let inputData = keyboard.availableData
+    let result = NSString(data: inputData, encoding:NSUTF8StringEncoding) as! String
+    
+    return result.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
+}
 
+println("Welcome to Alex Schenck's SimpleCalc! Enter your operation now.")
+let firstInput = input()
+println(firstInput)
