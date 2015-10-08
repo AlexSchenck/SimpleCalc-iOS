1000e50a919f89a02f9a69ef1b0f2d55ac68b491
Alex Schenck <AlexNSchenck@gmail.com>
2015-10-08 12:07:42 -0700

    Added extended operations and finished comments. Submitted

--- SimpleCalc/SimpleCalc/main.swift
+++ SimpleCalc/SimpleCalc/main.swift
@@ -4,6 +4,7 @@
 //
 //  Created by Alex Schenck on 10/7/15.
 //  Copyright (c) 2015 Alex Schenck. All rights reserved.
+//  INFO 498
 //
 
 import Foundation
@@ -17,40 +18,75 @@ func input() -> String {
     return result.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
 }
 
-
-// Converts given String to Int and returns result
-func convertToInt(incoming:String) -> Int {
-    return (incoming as NSString).integerValue
-}
-
+// Converts given String to Double and returns result
 func convertToDouble(incoming:String) -> Double {
     return (incoming as NSString).doubleValue
 }
 
 // Splits given String by spaces into an Array
-func splitBySpaces(incoming:String) -> [String]
-{
+func splitBySpaces(incoming:String) -> [String] {
     return incoming.componentsSeparatedByString(" ")
 }
 
-println("Welcome to Alex Schenck's SimpleCalc! Enter your operation now.")
+// Returns factorial of given Int
+func factorial(incoming:Double) -> Double {
+    if incoming < 2 {
+        return 1
+    }
+    else {
+        return incoming * factorial(incoming - 1)
+    }
+}
+
+println("Welcome to Alex Schenck's SimpleCalc! Now ready for input.")
 
 // First line entered by user split into an array by spaces
-var firstUserInput = splitBySpaces(input())
+var userInput = splitBySpaces(input())
 
 // There are multiple elements in the input, treat as multi-operand operation
-if (firstUserInput.count > 1) {
+if (userInput.count > 1) {
+    let op = userInput[userInput.count - 1]
     
+    switch op {
+        case "count":
+            // Count operation, return array size minus operator descriptor
+            println(userInput.count - 1)
+        case "avg":
+            // Average operation, add all numbers, divide by array size minus operator descriptor
+            let numberCount = userInput.count - 1
+            var result : Double = 0
+            var temp : Double = 0
+            for var index = 0; index < numberCount; index++ {
+                temp = convertToDouble(userInput[index])
+                result += temp
+            }
+
+            result = result / Double(numberCount)
+            println(result)
+        case "fact":
+            // Factorial operation, print error if given more than one number, otherwise return factorial
+            if (userInput.count > 2) {
+                println("Factorial operation only accepts one number")
+            }
+            else {
+                println(factorial(convertToDouble(userInput[0])))
+            }
+        default:
+            // Operation given in incorrect format or unrecognized operator
+            println("Not an accepted operator!")
+    }
 }
 else {
     // Only one element in first input, treat as simple multi-line calculation
     
     // Gather variables and convert to double
-    let first = convertToDouble(firstUserInput[0])
+    let first = convertToDouble(userInput[0])
     
+    // Operator
     println("Enter desired operation")
     let op = input()
     
+    // Second number
     println("Enter second number")
     let second = convertToDouble(input())
     
@@ -62,7 +98,7 @@ else {
     switch op {
         case "+":
             r = first + second
-            println(r);
+            println(r)
         case "-":
             r = first - second
             println(r)
@@ -76,6 +112,7 @@ else {
             r = first % second
             println(r)
         default:
+            // Operator not recognized
             println("Not an accepted operator!")
     }
 }
